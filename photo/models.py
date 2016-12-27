4497ce5e61bf8caafa3b16b71372db4bc8850ac0
Hyung Ryul Park <ryulstory@gmail.com>
2016-12-27 23:29:58 +0900

    date 12/27

--- photo/models.py
+++ photo/models.py
@@ -1,26 +1,28 @@
 from django.db import models
+
 class Categories(models.Model):
 	Title = models.CharField(max_length=40, null=False)
 
+class TagModel(models.Model)j:
+	Title = models.CharField(max_length=20,null=False)
+
 class Photo(models.Model) :
-	#분류
-	Categories = models.ForeignKey(Categories)
-	#틀린이유
-	Content = models.TextField(blank=True)
+	Category = models.ForeignKey(Categories)
+	Tags = models.ManyToManyField(TagModel)
 	image_file = models.ImageField(upload_to='static_files/uploaded/%Y')
 	filtered_image_file = models.ImageField(upload_to='static_files/uploaded/%Y')
 	description = models.TextField(max_length=500, blank=True)
+	Comments = models.PositiveSmallIntegerField(default=0, null=True)
 	created_at = models.DateTimeField(auto_now_add=True)
-	updated = models.DateTimeField(auto_now=True)
-	def delete(self,*args, **kwargs):
+	def delete(self, *args, **kwargs):
 		self.image_file.delete()
 		self.filtered_image_file.delete()
-		super(Photo, self).delete(*args,**kwargs)
+		super(Photo, self).delete(*args, **kwargs)
 class Comments(models.Model):
 	Name = models.CharField(max_length=20, null=False)
 	Content = models.TextField(max_length=2000, null=False)
-	Created = models.DateTimeField(auto_now_add=True, auto_now=True)
-	
-
+	created_at = models.DateTimeField(auto_now_add=True)
+	photo = models.ForeignKey(Photo)
+		
 
 # Create your models here.
--- photo/views.py
+++ photo/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-def single_photo(request) :
-	return HttpResponse('3번...3번 사진을 보여드릴게요')
+from wanote.photo.models import photo
 # Create your views here.
+
+#한개의 사진만 보기
+def single_photo(request, photo_id):
+	photo = photo.object.get(pk=photo_id)
+	response_text = '<p>{photo_id}번...{photo_id}번 사진을 보여 드릴게요.</p>'
+	response_text += '<p>{photo_url}</p>'
+
+	return HttpResponse(response_text.format(
+		photo_id=photo_id,
+		photo_url=photo.image_file.url)
+	)
+
+#목록보기
+def index(request, page=1, n):
+	#한페이지당 사진 갯수
+	per_page=n
+	start_pos=(page-1)*per_page
+	end_pos=start_pos+per_page
+	#시간 역순으로 정리
+	photo = photo.object.all().order_by('-created')[start_pos:end_pos]
+
